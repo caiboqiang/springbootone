b7c2fd8be281e03996a097a877e2a5e081a61711
908507670@qq.com <908507670@qq.com>
2018-05-11 17:28:28 +0800

    添加事物

--- src/main/java/com/service/HelpCategoryService.java
+++ src/main/java/com/service/HelpCategoryService.java
@@ -4,13 +4,16 @@ import com.mapper.HelpCategoryMapper;
 import com.pojo.HelpCategory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.xml.bind.annotation.XmlTransient;
 
 @Service
 public class HelpCategoryService {
 
     @Autowired
     private HelpCategoryMapper helpCategoryMapper;
-
+    @Transactional//事物 当其中一个不成功就回滚
     public HelpCategory getId(Short helpCategoryId){
 
         return helpCategoryMapper.selectByPrimaryKey(helpCategoryId);
