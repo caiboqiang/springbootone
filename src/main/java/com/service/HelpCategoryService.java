f1e8f1db4f5b112cd60f6981abf1b3335927ba7c
908507670@qq.com <908507670@qq.com>
2018-05-11 17:37:06 +0800

    测试

--- src/main/java/com/service/HelpCategoryService.java
+++ src/main/java/com/service/HelpCategoryService.java
@@ -6,8 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.xml.bind.annotation.XmlTransient;
-
+/**
+ *
+ */
 @Service
 public class HelpCategoryService {
 
