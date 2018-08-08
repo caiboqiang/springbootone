b4df94400f84f76458a49b6a790bc72fa02254e7
908507670@qq.com <908507670@qq.com>
2018-08-08 09:47:56 +0800

    1.pom.xml添加Scala配置 2.添加Scala demo Scala and java 混编开发

---
+++ src/main/scala/demo/test.scala
@@ -0,0 +1,42 @@
+package demo
+
+object test {
+
+  def main(args: Array[String]): Unit = {
+    /**
+      * 定义常量
+      */
+    var s = 5;
+    /**
+      * 定义变量
+      */
+    var x = 1;
+    /**
+      * 把判断后的值fu
+      */
+    var x1 = if (x>1){
+      8
+    }else{
+      9
+    }
+    println(x1);
+  }
+
+  for (i <- 1 to 100){
+    //println(i)
+  }
+
+  for (i <- 1 to 3; j <- 1 to 3 if (i != j) ){
+    println(i*10+j)
+  }
+
+  /**
+    * 这种写法不可行
+    */
+  /*var ds =  for( i <- 1 to 10){
+    yield i*10
+  }*/
+
+  var d = for( i <- 1 to 10) yield i*10
+ println(d)
+}
