8add3929c2eceb01c23c6f4924f93c0798f9d161
caiboqiang <908507670@qq.com>
2018-09-18 16:20:03 +0800

    学习scala MR pom添加spark0-core依赖

---
+++ MR/src/main/java/MCMapeer.java
@@ -0,0 +1,21 @@
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+
+public class MCMapeer extends Mapper <LongWritable,Text, Text,LongWritable>{
+    private Text key1 = new Text();
+    private LongWritable val = new LongWritable();
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String[] strings = value.toString().split("");
+        for(String str :strings){
+            key1.set(str);
+            val.set(1);
+            context.write(key1,val);
+        }
+    }
+}
+
